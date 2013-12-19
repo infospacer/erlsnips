626ee0fa24d28554c82a77e3b28dc2ff1b7c46cd
infospacer <infospacer@gmail.com>
2013-12-19 18:02:46 +1000

    changes in mr

--- src/mr.erl
+++ src/mr.erl
@@ -16,11 +16,20 @@ r(Module) ->
 %Compile and reload file
 c(Module) ->
 	Path = "./src/"++atom_to_list(Module)++".erl",
-	case ({ok,Module} == compile:file(Path,[{outdir, "./ebin"}])) of
-		true -> io:format("Module ~w is succesfully compiled~n",[Module]);
+	case compile:file(Path,[{outdir, "./ebin"}])of
+		{ok,Module} -> io:format("Module (~w) is succesfully compiled~n",[Module]);
 		_ -> error
+	end,
+	case code:purge(Module) of
+		true -> io:format("Purging code of (~w) is true  ~n",[Module]);
+		false -> io:format("Purging code  of (~w) is false ~n",[Module])
+	end,
+	case code:delete(Module) of
+		true -> io:format("Old code of (~w) is succesfully deleted ~n",[Module]);
+		false -> error
+	end,
+	case code:load_file(Module) of
+		{module, Module} -> io:format("Module (~w) is succesfully loaded ~n",[Module]);
+		{error, nofile} -> io:format("Module is not found ~n",[])
 	end.
-	% code:delete(Module),
-	% code:purge(Module),
-	% code:load_file(Module).
 
